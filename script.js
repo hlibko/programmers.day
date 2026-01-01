43f21603cad6e4750088172698cc0e9b3768fd16
FHK <72277730+hlibko@users.noreply.github.com>
2026-01-01 12:35:55 +0100

    Add a custom "Install App" button to the header

--- script.js
+++ script.js
@@ -70,3 +70,34 @@ if ('serviceWorker' in navigator) {
             });
     });
 }
+
+// Install Button Logic
+let deferredPrompt;
+const installBtn = document.getElementById('install-btn');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+    if (installBtn) {
+        installBtn.hidden = false;
+
+        installBtn.addEventListener('click', () => {
+            // Hide our user interface that shows our A2HS button
+            installBtn.hidden = true;
+            // Show the prompt
+            deferredPrompt.prompt();
+            // Wait for the user to respond to the prompt
+            deferredPrompt.userChoice.then((choiceResult) => {
+                if (choiceResult.outcome === 'accepted') {
+                    console.log('User accepted the A2HS prompt');
+                } else {
+                    console.log('User dismissed the A2HS prompt');
+                }
+                deferredPrompt = null;
+            });
+        });
+    }
+});
